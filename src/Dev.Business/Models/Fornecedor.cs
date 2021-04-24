e70d55d00b553edd52795935cc9d7f8126a4e061
Carlos Alberto <carlos98.camv@hotmail.com>
2021-04-24 11:02:32 -0300

    Criada as entidades do projeto

---
+++ src/Dev.Business/Models/Fornecedor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dev.Business.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedorEnum TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
---
+++ src/Dev.Business/Models/TipoFornecedorEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dev.Business.Models
+{
+    public enum TipoFornecedorEnum
+    {
+        PessoaFisica = 1,
+        PessoaJuridica
+    }
+}
