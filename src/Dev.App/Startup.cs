aeb9bbb30cb06d99d4a5f160926f86878b8a9b44
Carlos Alberto <carlos98.camv@hotmail.com>
2021-04-25 13:21:58 -0300

    Configurada as tabelas e colunas das Entidades

--- src/Dev.App/Startup.cs
+++ src/Dev.App/Startup.cs
@@ -1,17 +1,12 @@
 using Dev.App.Data;
+using Dev.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Dev.App
 {
@@ -24,19 +19,24 @@ namespace Dev.App
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<MeuDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
+
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -47,7 +47,6 @@ namespace Dev.App
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
@@ -60,9 +59,7 @@ namespace Dev.App
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
---
+++ src/Dev.Data/Mappings/FornecedorMapping.cs
@@ -0,0 +1,33 @@
+﻿using Dev.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dev.Data.Mappings
+{
+    public class FornecedorMapping : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.HasKey(f => f.Id);
+
+            builder.Property(f => f.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(f => f.Documento)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            builder.HasOne(f => f.Endereco)
+                .WithOne(e => e.Fornecedor)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+
+            builder.HasMany(f => f.Produtos)
+                .WithOne(p => p.Fornecedor)
+                .HasForeignKey(p => p.FornecedorId)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+
+            builder.ToTable("Fornecedores");
+        }
+    }
+}
